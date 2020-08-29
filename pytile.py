507763d19a46ed67006508590050ac13d242cccf
Alexander Photios <alexander.photios@student.tsc.nsw.edu.au>
2020-08-29 16:38:58 +1000

    Fixed More Git Errors

--- pytile.py
+++ pytile.py
@@ -119,26 +119,16 @@ def eColArrowHit():
     m = 0
     X = i[0]
     Y = i[1]
-<<<<<<< HEAD
 
-=======
->>>>>>> master
     eX = avatX
     eY = avatY
     eL = csizeX
     eH = csizeY
-<<<<<<< HEAD
-
+    
     if (eX < X < (eX + eL)) and (eY < Y < (eY + eH)):
         eMultipleArrows.remove(i)
         playerHealth = e[5] - playerHealth
-
-=======
-    if (eX < X < (eX + eL)) and (eY < Y < (eY + eH)):
-        eMultipleArrows.remove(i)
-        playerHealth = e[5] - playerHealth
-        
->>>>>>> master
+    
     if playerHealth <= 0:
         death = True
 
@@ -249,19 +239,7 @@ eRotateArrowDEG = 0
 eSingleArrow = []
 eMultipleArrows = []
 
-<<<<<<< HEAD
 avatar = 'Green Arrow'
-=======
-<<<<<<< Updated upstream
-avatar = 'The Flash'
-=======
-<<<<<<< HEAD
-avatar = 'Green Arrow'
-=======
-avatar = 'The Flash'
->>>>>>> EnemyShooting
->>>>>>> Stashed changes
->>>>>>> master
 projectilePic = 'arrowPic.png'
 standingR = 'arrowRunning.png'
 standingL = 'arrowRunningL.png'
@@ -457,24 +435,8 @@ while end == False:
 
                 eArrowX = e[0] + (asizeX / 2)
                 eArrowY = e[1] + (asizeY / 2)
-<<<<<<< HEAD
                 eArrowDifL = eArrowX - avatX - (csizeX/2)
                 eArrowDifH = eArrowY - avatY - (csizeY/2)
-=======
-
-<<<<<<< Updated upstream
-                eArrowDifL = eArrowX - avatX
-                eArrowDifH = eArrowY - avatY
-=======
-<<<<<<< HEAD
-                eArrowDifL = eArrowX - avatX - (csizeX/2)
-                eArrowDifH = eArrowY - avatY - (csizeY/2)
-=======
-                eArrowDifL = eArrowX - avatX
-                eArrowDifH = eArrowY - avatY
->>>>>>> EnemyShooting
->>>>>>> Stashed changes
->>>>>>> master
                 eRotateArrow = math.atan2(eArrowDifL, eArrowDifH)
                 eRotateArrowDEG = 90+(eRotateArrow * (180/math.pi))
                 
@@ -502,19 +464,7 @@ while end == False:
         i[1] = i[1] + (eArrowSpeed * (i[3]))
 
         colArrow()
-<<<<<<< HEAD
         eColArrowHit()
-=======
-<<<<<<< Updated upstream
-        # colArrowHit()
-=======
-<<<<<<< HEAD
-        eColArrowHit()
-=======
-        # colArrowHit()
->>>>>>> EnemyShooting
->>>>>>> Stashed changes
->>>>>>> master
 
         if i[1] <= 0 or i[1] >= 750 or i[0] <= 0 or i[0] >= 750 or arrowColidedWithBuilding == True:
             eMultipleArrows.remove(i)
